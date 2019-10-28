1ec32f887bb4946ac0f2f9de44a2137efb68ca4e
José Luis Valderrama Regueira <pepe.valderrama.regueira@gmail.com>
2019-10-28 15:45:36 +0100

    02 class finished
    Methods, v-model and click event

--- 01/01.js
+++ 01/01.js
@@ -6,6 +6,26 @@ const app = new Vue({
       { name: 'Web design', hoursPerDay: 4, doingNow: true },
       { name: 'Nature', hoursPerDay: 2, doingNow: false },
       { name: 'Longboarding', hoursPerDay: 1, doingNow: false }
-    ]
+    ],
+    newHobbie: {
+      name: '',
+      hoursPerDay: '',
+      doingNow: false
+    }
+  },
+  methods: {
+    // Add a new hobbie to the hobbies list.
+    addNewHobbie (e) {
+      e.preventDefault()
+      this.hobbies.push({
+        name: this.newHobbie.name,
+        hoursPerDay: this.newHobbie.hoursPerDay,
+        doingNow: this.newHobbie.doingNow
+      })
+
+      // Reset the new hobbie form
+      this.newHobbie.name = ''
+      this.newHobbie.hoursPerDay = ''
+    }
   }
 })
