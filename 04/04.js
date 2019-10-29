7a94b873c1f4d46ae000d43794c4e587535498c4
José Luis Valderrama Regueira <pepe.valderrama.regueira@gmail.com>
2019-10-29 22:43:19 +0100

    Update 04.html and 04.js to style the second div with computed properties

--- 04/04.js
+++ 04/04.js
@@ -3,5 +3,14 @@ const app = new Vue({
   data: {
     style: 'bg-default',
     quantity: 0
+  },
+  computed: {
+    quantityColor() {
+      return {
+        'bg-red' : this.quantity === 0,
+        'bg-blue' : this.quantity > 0 && this.quantity <= 4,
+        'bg-green' : this.quantity > 4
+      }
+    }
   }
 })
