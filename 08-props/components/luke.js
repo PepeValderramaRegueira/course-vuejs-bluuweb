20555417d8f7a8a6ca86ece926f27d6acc90d07a
José Luis Valderrama Regueira <pepe.valderrama.regueira@gmail.com>
2019-10-30 15:20:26 +0100

    08-2 class finished
    Send data from the child to the parent

--- 08-props/components/luke.js
+++ 08-props/components/luke.js
@@ -1,15 +1,29 @@
 Vue.component('luke', {
   props: ['legacy'],
 
+  mounted() {
+    this.$emit('lukeAffect', this.affect)
+  },
+  
   template: `
     <div class="luke">
       <p>{{status}}</p>
+      <p>I give your my {{checkAffect}}</p>
     </div>
   `,
+
+  data() {
+    return {
+      affect: 'hate'
+    }
+  },
 
   computed: {
     status() {
       return this.legacy === 'evil' ? "Nooooooo! 😱 I don't want your evilness!" : "Yesss! I want your force 😊"
+    },
+    checkAffect() {
+      return this.legacy === 'evil' ? 'hate 😠' : 'love ❤️'
     }
-  }
+  },
 })
--- 08-props/components/vader.js
+++ 08-props/components/vader.js
@@ -2,14 +2,16 @@ Vue.component('vader', {
   template: `
     <div class="vader" :class="status">
       <p>I am your father. I give you my {{vaderLegacy}}.</p>
-      <button class="goToTheLightSide-button" @click="goToTheLightSide()">Send Vader to the light side</button>
-      <luke :legacy="vaderLegacy"></luke>
+      <button v-if="vaderLegacy === 'evil'" class="goToTheLightSide-button" @click="goToTheLightSide()">Send Vader to the light side</button>
+      <luke :legacy="vaderLegacy" @lukeAffect="lukeAffect = $event"></luke>
+      <p>When I told you I was your father, you gave me your {{lukeAffect}}.</p>
     </div>
   `,
 
   data() {
     return {
-      vaderLegacy: 'evil'
+      vaderLegacy: 'evil',
+      lukeAffect: ''
     }
   },
 
