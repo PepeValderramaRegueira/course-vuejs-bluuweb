9075d849a0075b32ccdfced8d1a8bae47e02c558
José Luis Valderrama Regueira <pepe.valderrama.regueira@gmail.com>
2019-10-30 16:47:47 +0100

    09-2 class finished
    Communication between components using computed properties
    and Vuex (but in this case not mapping the state)

---
+++ 09-vuex-2/components/child.js
@@ -0,0 +1,17 @@
+Vue.component('child', {
+  template: `
+    <div class="child">
+      <h3>Child component</h3>
+      <p>Number in the child: {{number}}</p>
+      <button @click="$store.commit('increment')">+</button>
+      <span>Number value: </span>
+      <input v-model="$store.state.number">
+    </div>
+  `,
+  
+  computed: {
+    number() {
+      return store.state.number
+    }
+  }
+})
