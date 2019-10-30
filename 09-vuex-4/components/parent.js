55452052e03f55cefada9528f0f3d0c5dba3cbca
José Luis Valderrama Regueira <pepe.valderrama.regueira@gmail.com>
2019-10-30 17:05:08 +0100

    09-4 class finished
    Add mapMutations

---
+++ 09-vuex-4/components/parent.js
@@ -0,0 +1,13 @@
+Vue.component('parent', {
+  template: `
+    <div class="parent">
+      <h2>Parent component</h2>
+      <p>Number in the parent: {{number}}</p>
+      <child></child>
+    </div>
+  `,
+  
+  computed: {
+    ...Vuex.mapState(['number'])
+  }
+})
